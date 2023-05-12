c0b2db1dc6243a96e7d4f9f9fc8d6ea084c560fa
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2023-05-12 09:10:45 +0630

    ecommerce

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() {
+        $products = Product::all();
+        return view('admin.products.index', compact('products'));
+    }
+
+    public function add() {
+        $category = Category::all();
+        return view('admin.products.add', compact('category'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'isAdmin'])->group(function() {
+      
+    Route::get('/dashboard', 'App\Http\Controllers\Admin\FrontendController@index');
+
+    Route::get('categories', 'App\Http\Controllers\Admin\CategoryController@index');
+    Route::get('add-category', 'App\Http\Controllers\Admin\CategoryController@add');
+    Route::post('insert-category', 'App\Http\Controllers\Admin\CategoryController@insert');
+    Route::get('edit-prod/{id}', [CategoryController::class, 'edit']);
+    Route::put('update-category/{id}', [CategoryController::class, 'update']);
+    Route::get('delete-category/{id}', [CategoryController::class, 'destroy']);
+
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('add-products', [ProductController::class, 'add']);
+});
