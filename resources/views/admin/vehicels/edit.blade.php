649afa2330e9445de600e89a1daf7d9636a455c3
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2023-12-09 23:54:50 +0630

    laravelapi

--- resources/views/admin/vehicels/edit.blade.php
+++ resources/views/admin/vehicels/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            <h4>Edit/Update Products</h4>
+            <h4>Edit/Update Vehicels</h4>
         </div>
         <div class="card-body">
             <form action="{{ url('update-product/'.$products->id) }}" method="post" enctype="multipart/form-data">
@@ -27,49 +27,7 @@
                         <label for="">Small Description</label>
                         <textarea name="small_description"  rows="3" class="form-control">{{ $products->small_description }}</textarea>
                     </div>
-                    <div class="col-md-12 mb-3">
-                        <label for="">Description</label>
-                        <textarea name="description"  rows="3" class="form-control">{{ $products->description }}</textarea>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="">Original Price</label>
-                        <input type="number" class="form-control" value="{{ $products->original_price }}" name="original_price">
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="">Selling Price</label>
-                        <input type="number" class="form-control" value="{{ $products->selling_price }}" name="selling_price">
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="">Tax</label>
-                        <input type="number" value="{{ $products->tax }}" class="form-control" name="tax">
-                    </div>
-                    
-                    <div class="col-md-6 mb-3">
-                        <label for="">Quanlity</label>
-                        <input type="number" class="form-control" value="{{ $products->qty }}" name="qty">
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="">Status</label>
-                        <input type="checkbox" name="status" {{ $products->status  == '1'? 'checked' : '' }}>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="">Trending</label>
-                        <input type="checkbox" name="trending" {{ $products->trending  == '1'? 'checked' : '' }} >
-                    </div>
-
-                    <div class="col-md-12 mb-3">
-                        <label for="">Meta Title</label>
-                        <input type="text" class="form-control" value="{{ $products->meta_title }}" name="meta_title">
-                    </div>
-                    <div class="col-md-12 mb-3">
-                        <label for="">Meta Keywords</label>
-                        <textarea name="meta_keywords" rows="3" class="form-control">{{ $products->meta_keywords }}</textarea>
-                    </div>
-
-                    <div class="col-md-12 mb-3">
-                        <label for="">Meta Description</label>
-                        <textarea name="meta_description" rows="3" class="form-control">{{ $products->meta_description }}</textarea>
-                    </div>
+ 
                     @if ($products->image)
                         <img src="{{ asset('assets/uploads/product/'.$products->image) }}" class="cate-image" alt="Product image">
                     @endif
