c132ce41f511bef6008a626b9464809caf3cd119
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2023-12-13 23:52:51 +0630

    laravelapi

--- resources/views/admin/vehicels/add.blade.php
+++ resources/views/admin/vehicels/add.blade.php
@@ -26,28 +26,6 @@
                         <label for="">Description</label>
                         <textarea name="description"  rows="3" class="form-control"></textarea>
                     </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="">Original Price</label>
-                        <input type="number" class="form-control" name="original_price">
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="">Selling Price</label>
-                        <input type="number" class="form-control" name="selling_price">
-                    </div>
-
-                    <div class="col-md-12 mb-3">
-                        <label for="">Meta Title</label>
-                        <input type="text" class="form-control" name="meta_title">
-                    </div>
-                    <div class="col-md-12 mb-3">
-                        <label for="">Meta Keywords</label>
-                        <textarea name="meta_keywords" rows="3" class="form-control"></textarea>
-                    </div>
-
-                    <div class="col-md-12 mb-3">
-                        <label for="">Meta Description</label>
-                        <textarea name="meta_description" rows="3" class="form-control"></textarea>
-                    </div>
 
                     <div class="col-md-12">
                         <input type="file" name="image" class="form-control">
